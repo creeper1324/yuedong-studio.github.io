ccda4130751dd572b4e2eb60f139b6e40e39bb81
creeper1324 <130844986+creeper1324@users.noreply.github.com>
2024-09-17 20:12:46 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -2,43 +2,76 @@ function ani_home() {
     let menu = document.getElementById("menu");
     menu.classList.add("home");
     menu.classList.remove("project");
-    menu.classList.remove("blog");
     menu.classList.remove("support");
     menu.classList.remove("about");
+    menu.classList.remove("news");
 }
 
 function ani_project() {
     let menu = document.getElementById("menu");
     menu.classList.remove("home");
     menu.classList.add("project");
-    menu.classList.remove("blog");
     menu.classList.remove("support");
     menu.classList.remove("about");
-}
-
-function ani_blog() {
-    let menu = document.getElementById("menu");
-    menu.classList.remove("home");
-    menu.classList.remove("project");
-    menu.classList.add("blog");
-    menu.classList.remove("support");
-    menu.classList.remove("about");
+    menu.classList.remove("news");
 }
 
 function ani_support() {
     let menu = document.getElementById("menu");
     menu.classList.remove("home");
     menu.classList.remove("project");
-    menu.classList.remove("blog");
     menu.classList.add("support");
     menu.classList.remove("about");
+    menu.classList.remove("news");
 }
 
 function ani_about() {
     let menu = document.getElementById("menu");
     menu.classList.remove("home");
     menu.classList.remove("project");
-    menu.classList.remove("blog");
     menu.classList.remove("support");
     menu.classList.add("about");
+    menu.classList.remove("news");
 }
+
+function ani_news() {
+    let menu = document.getElementById("menu");
+    menu.classList.remove("home");
+    menu.classList.remove("project");
+    menu.classList.remove("support");
+    menu.classList.remove("about");
+    menu.classList.add("news");
+}
+
+function language_select_open() {
+    document.getElementById("language_option").classList.remove("open");
+    document.getElementById("language_option2").classList.remove("open");
+    document.getElementById("language_option3").classList.remove("open");
+    document.getElementById("language_select").classList.add("open");
+}
+
+function language_select_close() {
+    document.getElementById("language_option").classList.add("open");
+    document.getElementById("language_option2").classList.add("open");
+    document.getElementById("language_option3").classList.add("open");
+    document.getElementById("language_select").classList.remove("open");
+}
+
+function switchLanguage(language) {
+    var elements = document.querySelectorAll('[lang-en]');
+    elements.forEach(function(element) {
+        element.textContent = element.getAttribute('lang-' + language);
+    });
+}
+document.onkeydown = function (event) {
+    if (event.keyCode === 123) { // F12键的键码
+      event.preventDefault();
+      return false;
+    }
+};
+
+document.oncontextmenu = function (event) {
+    event.preventDefault();
+    return false;
+}
+
